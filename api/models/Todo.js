14a6adc6e8e8ca0a32148a9610b8f261ae9742cc
dinesh babu <dineshbabu853@gmail.com>
2023-04-05 22:36:44 +0530

    added template

--- api/models/Todo.js
+++ api/models/Todo.js
@@ -3,6 +3,11 @@ const schema = mongoose.Schema;
 
 const TodoSchema = new schema({
     // write your schema here
+    text: {
+        type: String,
+        required: true
+    },
+    // you can add more fields here
 });
 
 const Todo = mongoose.model('Todo', TodoSchema);
